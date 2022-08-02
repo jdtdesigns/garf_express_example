c3bb17dcb18393fb3310884349d786edfcb99541
JD Tadlock <jt7903@gmail.com>
2022-08-02 11:52:50 -0400

    set up server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const PORT = process.env.PORT || 3333;
+
+const app = express();
+
+app.use(express.json());
 
 
-
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
